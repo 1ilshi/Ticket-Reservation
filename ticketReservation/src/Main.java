6a79de5d791d2e6e062040af2cb3448823b82860
Muhammadrasheed Rida Ahmad Unus Ahmad Samadi <105810480+1ilshi@users.noreply.github.com>
2023-05-04 22:42:33 +0700

    Update Main.java

--- ticketReservation/src/Main.java
+++ ticketReservation/src/Main.java
@@ -5,30 +5,29 @@ public class Main {
         TheaterAndTime tat = new TheaterAndTime();
         SeatsProgramAndDetail sp = new SeatsProgramAndDetail();
 
-        // user login
-        if(new Login().start()){
-            // receive date from user
-            d.start();
-
-            // receive movie that user chose
-            mp.start();
-            String m = mp.getMovie();
-
-            // get user chooses's theater
-            tat.start(m);
-
-            // receive seat type and ticket amount from user
-            sp.start();
-
-            // show reservation detail -> date, movie, theater, time, seat, total price
-            d.Detail();
-            mp.Detail();
-            tat.Detail();
-            sp.Detail();
-
-            // get payment method which user chose
-            new PaymentMethods().start();
+        while(!new Login().start()){
+            // user login
         }
+        // receive date from user
+        d.start();
+
+        // receive movie that user chose
+        mp.start();
+        String m = mp.getMovie();
+
+        // get user chooses's theater
+        tat.start(m);
+
+        // receive seat type and ticket amount from user
+        sp.start();
+
+        // show reservation detail -> date, movie, theater, time, seat, total price
+        d.Detail();
+        mp.Detail();
+        tat.Detail();
+        sp.Detail();
+
+        // get payment method which user chose
+        new PaymentMethods().start();
     }
 }
-
